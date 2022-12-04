4508a241469f192dbf7889055c539efb463da18a
mulborg <toxin6@gmail.com>
2022-12-04 10:55:56 +0100

    Add solution of day 4 in 2022

---
+++ 2022/src/day4.rs
@@ -0,0 +1,113 @@
+use std::fs;
+use std::io::Read;
+
+pub fn day_4() {
+    let mut file = fs::File::open("./input_files/day4_input.txt").expect("File not found");
+    let mut data = String::new();
+    file.read_to_string(&mut data)
+        .expect("Error while reading file");
+    println!(
+        "Part 1: count of assignment pairs where one range fully contains the other: {}",
+        count_fully_contained_assignment_pairs(&data)
+    );
+    println!(
+        "Part 2: count of assignment pairs where ranges overlap: {}",
+        count_overlapped_assignment_pairs(&data)
+    );
+}
+
+fn count_fully_contained_assignment_pairs(input: &String) -> u16 {
+    let mut counter = 0;
+    for mut line in input.lines() {
+        line = line.trim();
+        if !line.is_empty() {
+            let pair: Vec<&str> = line.split(',').collect();
+            if pair.len() == 2 {
+                let section1 = parse_section(pair[0]);
+                let section2 = parse_section(pair[1]);
+                let sections_merged = section1 | section2;
+                if sections_merged == section1 || sections_merged == section2 {
+                    counter += 1;
+                }
+            }
+        }
+    }
+    counter
+}
+
+fn count_overlapped_assignment_pairs(input: &String) -> u16 {
+    let mut counter = 0;
+    for mut line in input.lines() {
+        line = line.trim();
+        if !line.is_empty() {
+            let pair: Vec<&str> = line.split(',').collect();
+            if pair.len() == 2 {
+                let section1 = parse_section(pair[0]);
+                let section2 = parse_section(pair[1]);
+                if section1 & section2 != 0 {
+                    counter += 1;
+                }
+            }
+        }
+    }
+    counter
+}
+
+fn parse_section(input: &str) -> u128 {
+    let section: Vec<&str> = input.split('-').collect();
+    if section.len() != 2 {
+        panic!("Invalid section!");
+    }
+    let start: u8 = section[0].parse().unwrap();
+    let end: u8 = section[1].parse().unwrap();
+    let mut id = 0;
+    for i in start..=end {
+        id = id | (1 << i);
+    }
+    id
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day4::{
+        count_fully_contained_assignment_pairs, count_overlapped_assignment_pairs, parse_section,
+    };
+
+    #[test]
+    fn test_count_fully_contained_assignment_pairs() {
+        let input = String::from(
+            "2-4,6-8
+            2-3,4-5
+            5-7,7-9
+            2-8,3-7
+            6-6,4-6
+            2-6,4-8",
+        );
+        assert_eq!(count_fully_contained_assignment_pairs(&input), 2);
+    }
+
+    #[test]
+    fn test_count_overlapped_assignment_pairs() {
+        let input = String::from(
+            "2-4,6-8
+            2-3,4-5
+            5-7,7-9
+            2-8,3-7
+            6-6,4-6
+            2-6,4-8",
+        );
+        assert_eq!(count_overlapped_assignment_pairs(&input), 4);
+    }
+
+    #[test]
+    fn test_parse_section() {
+        let mut input = String::from("2-4");
+        assert_eq!(parse_section(&input), 0b0001_1100);
+        input = String::from("6-6");
+        assert_eq!(parse_section(&input), 0b0100_0000);
+        input = String::from("7-6");
+        assert_eq!(parse_section(&input), 0b0000_0000);
+        input = String::from("0-127");
+        assert_eq!(parse_section(&input), !0b0);
+    }
+}
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,12 +1,14 @@
 use crate::day1::day_1;
 use crate::day2::day_2;
 use crate::day3::day_3;
+use crate::day4::day_4;
 use colored::Colorize;
 use std::env;
 
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     println!("{}", "Advent of Code 2022".green());
@@ -25,10 +27,12 @@ fn main() {
             return;
         }
     };
+    println!("{} {}:", "Day".yellow(), day.to_string().yellow());
     match day {
         1 => day_1(),
         2 => day_2(),
         3 => day_3(),
+        4 => day_4(),
         _ => show_error(message),
     }
 }
